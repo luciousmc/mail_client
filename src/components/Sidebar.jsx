f789c2f97a47b45d8da0f7530ce6eaf915ec4124
Marlon Clay <luciousmc@icloud.com>
2021-09-08 15:30:30 -0700

    sidebar options added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
-import { Button } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+
+import { Button } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
-import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
   return (
@@ -15,7 +22,13 @@ function Sidebar() {
         Compose
       </Button>
 
-      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected />
+      <SidebarOption Icon={StarIcon} title="Starred" number={14} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={14} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={14} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={14} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={14} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={14} />
     </div>
   );
 }
