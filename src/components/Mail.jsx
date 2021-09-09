81228beb0c7738712dcff8f43534269eef8f8824
Marlon Clay <luciousmc@icloud.com>
2021-09-08 20:07:17 -0700

    adds tool icons to mail component

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import './Mail.css'
+import React from "react";
+import "./Mail.css";
+
+import { IconButton } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ErrorIcon from "@material-ui/icons/Error";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import LabelImportant from "@material-ui/icons/LabelImportant";
+import MoreVert from "@material-ui/icons/MoreVert";
+
 
 function Mail() {
   return (
-    <div>
-      <h1>Mail</h1>
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton>
+              <ArrowBackIcon />
+            </IconButton>
+            
+            <IconButton>
+              <MoveToInboxIcon />
+            </IconButton>
+            
+            <IconButton>
+              <ErrorIcon />
+            </IconButton>
+
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+            
+            <IconButton>
+              <EmailIcon />
+            </IconButton>
+            
+            <IconButton>
+              <WatchLaterIcon />
+            </IconButton>
+            
+            <IconButton>
+              <LabelImportant />
+            </IconButton>
+            
+            <IconButton>
+              <MoreVert />
+            </IconButton>
+        </div>
+        
+        <div className="mail__toolsright"></div>
+      </div>
+
+
     </div>
-  )
+  );
 }
 
-export default Mail
+export default Mail;
